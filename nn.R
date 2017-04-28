85a30e11f46eebd72a0130ef2d8aed49b9e57750
Azad Memon <azad.memon@gmail.com>
2017-04-27 20:00:36 -0400

    Updated back propagation. Working on getting parameters for optimzing function correct

--- nn.R
+++ nn.R
@@ -8,61 +8,79 @@
 # TODO: Implement analysis 
 
 # Read in testing data
-testData <- read.csv("test.csv")
-testDataX <- t(testData[,-1])
-testDataY <- t(testData[,1])
+#testData <- read.csv("test.csv")
+#testDataX <- t(testData[,-1])
+#testDataY <- t(testData[,1])
 
 # Read in training data
 trainData <- read.csv("train.csv")
-trainDataX <- t(trainData[,-1])
-trainDataY <- t(trainData[,1])
+trainDataX <- t(trainData[1:5,-1])
+trainDataY <- t(trainData[1:5,1])
 
-costFunction <- function (predictedY, y, theta1, theta2, lambda) {
-  # TODO: Change this so theta1 and theta2 will be unrolled, and have to be rebuilt
-  m = nrow(predictedY)
-  cost = 0;
+fr <- function(X, Y, lambda, par) {
+  # reroll parmeters
+  theta1 <- matrix(head(par,615440), 784)
+  theta2 <- matrix(tail(par,-615440), 10)
   
-  # Main component
-  for (i in range(1, 10)) { # TODO: Fix this for loop, range isn't right
-    yBinary <- 1*(y[,i]==i)
-    cost = cost + colSums(rowSums((yBinary*log(predictedY) + (1-yBinary)*log(1-predictedY)))) 
+  output <- forwardPropagation(X, theta1, theta2)
+  a2 = output$a2
+  a3 = output$a3
+  
+  
+  predictedY <- a3 # Output layer is the predicted values
+  numTrainingExamples <- ncol(predictedY)
+  # Compute the cost function
+  cost <- 0;
+  
+  for (i in c(0:9)) {
+    yBinary <- 1*(Y==i)
+    cost <- cost + sum(rowSums(sweep(log(predictedY), MARGIN=2, yBinary, `*`) + sweep(log(1-predictedY), MARGIN=2, (1-yBinary), `*`)))
   }
   
   # Regularized component
-  cost = cost + lambda/(2*m) * (colSums(rowSums(theta1 * theta1)) + colSums(rowSums(theta2 * theta2)))
+  cost <- cost + lambda/(2*numTrainingExamples) * (sum(rowSums(theta1 * theta1)) + sum(rowSums(theta2 * theta2)))
+  return(cost)
 }
 
 # Similar to gradient descent, R implements the sigmoid function as well
 sigmoid <- function(z) {
-  1/(1 + exp(-1*z)) 
+  return(1/(1 + exp(-1*z)))
 }
 
-forwardPropagation <- function(X, y, theta1, theta2) {
+forwardPropagation <- function(X, theta1, theta2) {
   # compute activation nodes for second layer
-  XwithBias = cbind(1, X) # Add bias
-  a2 = sigmoid(t(theta1) %*% XwithBias)
+  XwithBias <- rbind(1, X) # Add bias
+  a2 <- sigmoid(theta1 %*% XwithBias)
   
   # compute activation nodes for third layer
-  a2WithBias = c(1, a2) # Add bias
-  a3 = sigmoid(t(theta2) %*% a2WithBias)
+  a2WithBias <- rbind(1, a2) # Add bias
+  a3 <- sigmoid(theta2 %*% a2WithBias)
   
-  list(a2, a3)
+  return(list("a2" = a2, "a3" = a3))
 }
 
-backPropagation <- function(theta1, theta2, X, a2, a3, y, lambda) {
-  num_examples = ncol(X)
+grr <- function(X, Y, lambda, par) {
+  # reroll parmeters
+  theta1 <- matrix(head(par,615440), 784)
+  theta2 <- matrix(tail(par,-615440), 10)
   
-  s3 = a3 - y
-  s2 = (t(theta2) %*% s3) * a2 * (1 - a2);
+  output <- forwardPropagation(X, theta1, theta2)
+  a2 = output$a2
+  a3 = output$a3
   
-  delta1 = s2 %*% t(a1)
-  delta2 = s3 %*% t(a2)
+  numExamples <- ncol(X)
+  
+  s3 <- sweep(a3, MARGIN=2, Y, `-`)
+  s2 <- (t(theta2[,-1]) %*% s3) * a2 * (1 - a2)
+  
+  delta1 <- s2 %*% t(rbind(1, X))
+  delta2 <- s3 %*% t(rbind(1, a2))
   
   
-  D1 = 1/num_examples * cbind(delta1[,1], delta1[,-1] + lambda*theta1[,-1])
-  D2 = 1/num_examples * cbind(delta2[,1], delta2[,-1] + lambda*theta2[,-1])
+  D1 <- 1/numExamples * cbind(delta1[,1], delta1[,-1] + lambda*theta1[,-1])
+  D2 <- 1/numExamples * cbind(delta2[,1], delta2[,-1] + lambda*theta2[,-1])
   
-  cbind(unlist(D1), unlist(D2))
+  return(c(as.vector(D1), as.vector(D2)))
 }
 
 # ----------------
@@ -76,22 +94,17 @@ backPropagation <- function(theta1, theta2, X, a2, a3, y, lambda) {
 numActivationUnits1 <- 785 # Includes bias unit
 numActivationUnits2 <- 784 # Does not include bias unit
 epsilon1 = sqrt(6)/sqrt(numActivationUnits2 + numActivationUnits1)
-theta1 <- 2*epsilon1*matrix(rnorm(numActivationUnits2*numActivationUnits1, mean=0, std=1), numActivationUnits2, numActivationUnits1) - epsilon1
+theta1 <- 2*epsilon1*matrix(rnorm(numActivationUnits2*numActivationUnits1, mean=0), numActivationUnits2, numActivationUnits1) - epsilon1
 
 # Randomly initialize weights for layer 2
 numActivationUnits2 <- 785 # Includes bias unit
 numActivationUnits3 <- 10 # Does not include bias unit
 epsilon2 = sqrt(6)/sqrt(numActivationUnits3 + numActivationUnits2)
-theta2 <- 2*epsilon2*matrix(rnorm(numActivationUnits3*numActivationUnits2, mean=0, std=1), numActivationUnits3, numActivationUnits2) - epsilon2
-
-# Forward propagation
-list[a2, a3] = forwardPropagation(trainDataX, trainDataY, theta1, theta2)
-
-# TODO, this should be a function passed into grad descent
-# Backpropagation
-lambda = 5 # TODO: Update lambda value to be appropriate
-list[d1, d2] = backPropagation(theta1, theta2, trainDataX, a2, a3, trainDataY, lambda)
+theta2 <- 2*epsilon2*matrix(rnorm(numActivationUnits3*numActivationUnits2, mean=0), numActivationUnits3, numActivationUnits2) - epsilon2
 
 # Gradient Descent
-optim(cbind(unlist(theta1), unlist(theta2)), fn = costFunction, gr = backPropagation)
+lambda <- 3
+init_theta <- c(as.vector(theta1), as.vector(theta2)) # Unrolled theta parameters
+result <- optim(par = init_theta, fr, grr, X = trainDataX, Y = trainDataY, lambda = lambda, method="BFGS", control=list("maxit"=25, "ndeps"=0.1, "reltol"=0.0001))
 
+print(result)
